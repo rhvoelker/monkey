a6093ff367efd0a723c5322fa0a0f0353210c51c
Richard Voelker <ricky.voelker@gmail.com>
2026-07-04 11:30:45 -0400

    Add Token code from section 1.2.

---
+++ token/token.go
@@ -0,0 +1,34 @@
+package token
+
+type Type string
+
+type Token struct {
+	Type    Type
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers
+	IDENT = "IDENT"
+	INT   = "INT"
+
+	// Operators
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
