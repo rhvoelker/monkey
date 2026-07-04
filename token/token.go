e990333a5ab31b1b7dbeb68aca93c5b036dc6c8c
Richard Voelker <ricky.voelker@gmail.com>
2026-07-04 12:33:45 -0400

    Add lexer from section 1.3.

--- token/token.go
+++ token/token.go
@@ -32,3 +32,15 @@ const (
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
 )
+
+var keywords = map[string]Type{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+func LookupIdent(ident string) Type {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
